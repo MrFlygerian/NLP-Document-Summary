8b7413fa1018ad17839119016390c2ef44cdf231
Blessed Chianumba <62527934+MrFlygerian@users.noreply.github.com>
2020-07-02 23:56:35 +0100

    Initial commit

---
+++ Summarizing word docs.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  2 16:08:48 2020
+
+@author: bless
+"""
+
+################################    SCRIPT TO BE CREATED FOR SUMMARY GENERATION   ###################################
