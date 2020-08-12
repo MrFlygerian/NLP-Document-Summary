d2389aa8a35002cc5c7ca2d066842db958ea0c24
Blessed Chianumba <62527934+MrFlygerian@users.noreply.github.com>
2020-08-12 15:30:53 +0100

    Refactored repo

--- scripts/Experiment Document Summary.py
+++ scripts/Experiment Document Summary.py
@@ -1,11 +1,11 @@
-#specific to extracting information from word documents
+# specific to extracting information from word documents
 import pathlib
 from get_docx_text import get_docx_text
 
-#converting letters to numbers
+# converting letters to numbers
 import numpy as np
 
-#deep learning modules
+# deep learning modules
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -13,17 +13,17 @@ from keras.layers import LSTM
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
 
-#alternative to print
+# alternative to print
 import sys
 
-#load files and extract text
+# load files and extract text
 FILE_PATH = r'C:\Users\bless\OneDrive\Documents\Goals Docs\Reading docs'
 source = pathlib.Path(FILE_PATH)
 files = [s for s in source.iterdir()]
 texts = [get_docx_text(file).lower() for file in files]
 train_text = ' '.join(texts)
 
-#create numberical map using document characters
+# create numberical map using document characters
 chars = sorted(list(set(train_text)))
 char_to_int = dict((c, i) for i, c in enumerate(chars))
 int_to_char = dict((i, c) for i, c in enumerate(chars))
@@ -37,10 +37,10 @@ seq_length = 100
 dataX = []
 dataY = []
 for i in range(0, n_chars - seq_length, 1):
-	seq_in = train_text[i:i + seq_length]
-	seq_out = train_text[i + seq_length]
-	dataX.append([char_to_int[char] for char in seq_in])
-	dataY.append(char_to_int[seq_out])
+    seq_in = train_text[i:i + seq_length]
+    seq_out = train_text[i + seq_length]
+    dataX.append([char_to_int[char] for char in seq_in])
+    dataY.append(char_to_int[seq_out])
     
 n_patterns = len(dataX)
 
@@ -56,23 +56,28 @@ y = np_utils.to_categorical(dataY)
 
 # define the LSTM model
 model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+model.add(LSTM(256, input_shape=(X.shape[1],X.shape[2]),
+               return_sequences=True))
+
 model.add(Dropout(0.2))
 model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
-model.load_weights('control-starting-weight.hdf5')
+
+model.load_weights('experiment-starting-weight.hdf5')
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-#define checkpoint
-filepath="custom-weights-{epoch:02d}-{loss:.4f}.hdf5"
-checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-callbacks_list = [checkpoint]
+# define checkpoint
+filepath = "control-weights-{epoch:02d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss',
+                             verbose=1, save_best_only=True, mode='min')
 
-#-----------------------------------------------COMMENT OUT IF NOT GENERATING WEIGHTS---------------------------------------------------------------------------
-#fit model
-#model.fit(X, y, epochs=2, batch_size=60, callbacks=callbacks_list)
-#-----------------------------------------------COMMENT OUT IF NOT GENERATING WEIGHTS---------------------------------------------------------------------------
+callbacks_list = [checkpoint]
+# -----------------------------------------------COMMENT OUT IF NOT GENERATING WEIGHTS---------------------------------------------------------------------------
+# fit model
+# model.fit(X, y, epochs=2, batch_size=60,
+#            callbacks=callbacks_list)
+# -----------------------------------------------COMMENT OUT IF NOT GENERATING WEIGHTS---------------------------------------------------------------------------
 
 # pick a random seed
 start = np.random.randint(0, len(dataX)-1)
@@ -81,7 +86,7 @@ print ("Seed:")
 print ("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
 
 # generate characters
-for i in range(300):
+for i in range(500):
 	x = np.reshape(pattern, (1, len(pattern), 1))
 	x = x / float(n_vocab)
 	prediction = model.predict(x, verbose=0)
@@ -92,32 +97,3 @@ for i in range(300):
 	pattern.append(index)
 	pattern = pattern[1:len(pattern)]
 print ("\nDone.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
